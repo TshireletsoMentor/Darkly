bf6e39559dafa32630e310097ba52d3e0c7d28dd
TshireletsoMentor <tmentor@student.wethinkcode.co.za>
2020-10-16 18:00:03 +0200

    just need prevention

--- 06_Spoof_(cURL)/resource/referer.sh
+++ 06_Spoof_(cURL)/resource/referer.sh
@@ -1,3 +1,12 @@
-IP=
+IP=`head -n 1 '../../src/env_IP'`
+IP_len=${#IP}
+if [[ IP_len -gt 0 ]]
+then
+  continue
+else
+  echo "Input server IP address:" 
+  read IP
+fi
+
 curl -H "User-Agent: ft_bornToSec" --referer https://www.nsa.gov/ http://$IP?page=e43ad1fdc54babe674da7c7b8f0127bde61de3fbe01def7d00f151c2fcca6d1c |
  grep "flag"
